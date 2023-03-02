ee5b3f46569081f8990064943f88bad4a58030fa
Mozart <peterkayk@gmail.com>
2023-03-03 00:37:12 +0300

    Adding update button

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
     const inputRef = useRef(null)
 
@@ -27,8 +27,11 @@ function TodoForm(props) {
     return (
         <div>
             <form className='todo-form' onSubmit={handleSubmit}>
-                <input type='text' placeholder='Add a Todo' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef}/>
-                <button className='todo-button'>Add Todo</button>
+                {props.edit ? (
+                                    <><input type='text' placeholder='Update item' value={input} name='text' className='todo-input edit' onChange={handleChange} ref={inputRef} /><button className='todo-button eidit'>Update</button></>
+                ) : (
+                    <><input type='text' placeholder='Add a Todo' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef} /><button className='todo-button'>Add Todo</button></>
+                )}
             </form>
         </div>
     )
